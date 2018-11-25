e73cb5bdce5c48b00c65512db03248de2a9ad18b
Joeyipp <nickyip92@gmail.com>
2018-11-24 19:45:41 -0500

    Testing getNutrition intent V11

--- app.js
+++ app.js
@@ -47,7 +47,7 @@ app.post('/webhook', function (req, res) {
         var defaultFulfillmentMessage = req.body.queryResult.fulfillmentMessages;
 
         var getResponseObj = async (userQuery) => {
-            const foodInfo = await getNutrition(userQuery);
+            const foodInfo = await nutrition.getNutrition(userQuery);
             const food = foodInfo.food;
             const food_sum = foodInfo.food_sum;
             nutrition_description = `Amount Per Serving\nCalories: ${food_sum.sum_calories.toFixed()}\nTotal Fat: ${food_sum.sum_total_fat.toFixed(1)}g\nCholesterol: ${food_sum.sum_cholesterol.toFixed(1)}mg\nSodium: ${food_sum.sum_sodium.toFixed()}mg\nPotassium: ${food_sum.sum_potassium.toFixed()}mg\nTotal Carbohydrates: ${food_sum.sum_total_carbohydrates.toFixed()}g\n  Dietary Fiber: ${food_sum.sum_fibre.toFixed(1)}g\n  Sugars: ${food_sum.sum_sugar.toFixed(1)}g\nProtein: ${food_sum.sum_protein.toFixed(1)}g`;
