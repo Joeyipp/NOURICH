bb10129291776c902e3f0083876e37cb602531c8
Joeyipp <nickyip92@gmail.com>
2018-11-25 21:15:04 -0500

    Updated user account management

--- server/app.js
+++ server/app.js
@@ -73,13 +73,14 @@ app.post('/webhook', function (req, res) {
         })
     }
     else if (intent == "User Signup Health Condition") {
-        var name = req.body.queryResult.outputContexts[0][parameters]["given-name"];
-        var age = req.body.queryResult.outputContexts[0][parameters]["age"];
-        var height = req.body.queryResult.outputContexts[0][parameters]["unit-length.original"];
-        var weight = req.body.queryResult.outputContexts[0][parameters]["unit-weight.original"];
-        var diet_plan = req.body.queryResult.outputContexts[0][parameters]["Diet_plan"];
-        var food_allergies = req.body.queryResult.outputContexts[0][parameters]["food_allergies"];
-        var health_condition = req.body.queryResult.outputContexts[0][parameters]["health_condition"];
+        console.log(req.body.queryResult.outputContexts[0])
+        var name = req.body.queryResult.outputContexts[0].parameters["given-name"];
+        var age = req.body.queryResult.outputContexts[0].parameters["age"];
+        var height = req.body.queryResult.outputContexts[0].parameters["unit-length.original"];
+        var weight = req.body.queryResult.outputContexts[0].parameters["unit-weight.original"];
+        var diet_plan = req.body.queryResult.outputContexts[0].parameters["Diet_plan"];
+        var food_allergies = req.body.queryResult.outputContexts[0].parameters["food_allergies"];
+        var health_condition = req.body.queryResult.outputContexts[0].parameters["health_condition"];
 
         console.log(name, age, height, weight, diet_plan, food_allergies, health_condition);
 
